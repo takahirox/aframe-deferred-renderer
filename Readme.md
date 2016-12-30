0b416ac6731bb1cd222be403524c42d96ef361e3
Takahiro <hogehoge@gachapin.jp>
2016-12-29 16:56:04 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,62 @@
+# A-Frame Deferred Renderer component
+
+aframe-deferred-renderer is an A-Frame Deferred Renderer component which enables MMD models to dance.
+
+Deferred Rendering achieves a good performance with many lights.
+
+![screenshot](./screenshot.png "screenshot")
+
+## Properties
+
+### aframe-deferred-renderer
+
+| Properties   | type    | Default Value | Description |
+| ------------ | ------- | ------------- | ----------- |
+| lightPrePass l boolean | true          | If true light pre-pass mode is used. Otherwise classic mode is used. |
+
+## Browser
+
+### How to use
+
+```
+<head>
+  <script src="./libs/aframe.min.js"></script>
+  <script src="./build/aframe-deferred-renderer.min.js"></script>
+</head>
+
+<body>
+  <a-scene deferred-renderer>
+    <a-entity position="0 10 20">
+      <a-camera></a-camera>
+    </a-entity>
+
+    <a-entity geometry="primitive:sphere"></a-entity>
+
+    <a-entity light="type:point;color:#AAA;distance:10" position="0 0 0"></a-entity>
+    <a-entity light="type:point;color:#AAA;distance:10" position="10 10 10"></a-entity>
+    <a-entity light="type:point;color:#AAA;distance:10" position="-10 -10 -10"></a-entity>
+  </a-scene>
+</body>
+```
+
+## NPM
+
+### How to install
+
+```
+$ npm install aframe-deferred-renderer
+```
+
+### How to build
+
+```
+$ npm install
+$ npm run all
+```
+
+### How to load
+
+```
+require('aframe');
+require('aframe-deferred-renderer');
+```
---
+++ index.js
@@ -0,0 +1,62 @@
+/**
+ * @author Takahiro / https://github.com/takahirox
+ */
+
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before' +
+                  'AFRAME was available.');
+}
+
+require('three/examples/js/shaders/CopyShader');
+require('three/examples/js/shaders/FXAAShader');
+require('three/examples/js/postprocessing/EffectComposer');
+require('three/examples/js/postprocessing/RenderPass');
+require('three/examples/js/postprocessing/ShaderPass');
+require('three/examples/js/renderers/WebGLDeferredRenderer');
+
+AFRAME.registerComponent('deferred-renderer', {
+  schema: {
+    lightPrePass: {type: 'boolean', default: true}
+  },
+
+  init: function () {
+    this.renderer = null;
+  },
+
+  update: function () {
+    if (this.renderer !== null) { return; }
+
+    var data = this.data;
+    var el = this.el;
+    var sceneEl = el.sceneEl;
+    var renderer = sceneEl.renderer;
+
+    if (renderer === undefined) { return; }
+
+    var size = renderer.getSize();
+    var newRenderer = new THREE.WebGLDeferredRenderer({renderer:renderer});
+    newRenderer.enableLightPrePass(data.lightPrePass);
+    newRenderer.setSize(size.width, size.height);
+
+    var keys = Object.keys(renderer);
+    for (var i = 0, il = keys.length; i < il; i++) {
+      var key = keys[i];
+      if (newRenderer[key] === undefined) {
+        newRenderer[key] = typeof renderer[key] === 'function'
+                          ? renderer[key].bind(renderer)
+                          : renderer[key];
+      }
+    }
+
+    // override scene renderer and effect
+    // this's very sensitive to sceneEl impl
+    this.renderer = newRenderer;
+    sceneEl.renderer = newRenderer;
+    sceneEl.effect = new THREE.VREffect(this.renderer);
+  },
+
+  play: function () {
+    // just in case
+    this.update();
+  }
+});
